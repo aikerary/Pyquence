00e8b7b5b15cdde69f94afa3287d37c605a96b51
Aiker Acosta <aikiringo26@gmail.com>
2022-08-23 07:05:20 -0500

    Adding Pythons script for solving equations

---
+++ python/pyquence.py
@@ -0,0 +1,23 @@
+import sympy as sp
+
+# Create a function using sympy for extract the roots of an equation
+def roots(equation):
+    # Create a variable for the equation
+    eq = sp.sympify(equation)
+    # Create a variable for the roots
+    roots = sp.solve(eq)
+    # Return the roots
+    return roots
+
+# Create a def main and prove the roots works
+def main():
+    print(roots("x**7-10*x**6+28*x**5+30*x**4-297*x**3+540*x**2-324*x"))
+
+# If the file is run directly, run the main function.
+if __name__ == "__main__":
+    main()
+
+
+
+
+
