26fed7bb515d25e6e1936a513c2cf9664bb58171
Aiker Acosta <aikiringo26@gmail.com>
2022-08-23 17:47:47 -0500

    Stablish the final structure

--- python/pyquence.py
+++ python/pyquence.py
@@ -1,4 +1,6 @@
+from numbers import Real
 import sympy as sp
+import re
 
 # Create a function using sympy for extract the roots of an equation
 def roots(equation):
@@ -23,6 +25,21 @@ def main():
     x = sp.Symbol('x')
     print(roots("x**7-10*x**6+28*x**5+30*x**4-297*x**3+540*x**2-324*x"))
     print(factorize("x**7-10*x**6+28*x**5+30*x**4-297*x**3+540*x**2-324*x"))
+
+def finalSum(nrd, mj, rootlist):
+    n= sp.Symbol('n', real=True)
+    pyquation=""
+    equation = "("
+    bstring = "b"
+    for j in range(nrd):
+        for i in range(mj):
+            equation+="+b_"+str(i+mj-1)+"n**"+str(i-1)
+        equation+=")"+rootlist[j]+"**n"
+        pyquation+=equation
+        equation="+("
+    pyquationS = sp.sympify(pyquation)
+    return pyquationS
+            
 
 # If the file is run directly, run the main function.
 if __name__ == "__main__":
